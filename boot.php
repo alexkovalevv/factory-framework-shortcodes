e0fbb91fd8330f039385fe3f0bfcad2b34a1f5fe
alex.kovalevv <alex.kovalevv@gmail.com>
2019-07-30 05:49:18 +0300

    Обновлена версия модуля. Добавлена константа с версией модуля.

--- boot.php
+++ boot.php
@@ -1,22 +1,25 @@
 <?php
-	/**
-	 * Factory Shortcodes
-	 *
-	 * @author Alex Kovalev <alex.kovalevv@gmail.com>
-	 * @copyright (c) 2018, Webcraftic Ltd
-	 *
-	 * @package factory-shortcodes
-	 * @since 1.0.0
-	 */
+/**
+ * Factory Shortcodes
+ *
+ * @author        Alex Kovalev <alex.kovalevv@gmail.com>
+ * @since         1.0.0
+ * @package       factory-shortcodes
+ * @copyright (c) 2018, Webcraftic Ltd
+ *
+ */
 
-	if( defined('FACTORY_SHORTCODES_000_LOADED') ) {
-		return;
-	}
-	define('FACTORY_SHORTCODES_000_LOADED', true);
+if ( defined( 'FACTORY_SHORTCODES_000_LOADED' ) ) {
+	return;
+}
 
-	define('FACTORY_SHORTCODES_000_DIR', dirname(__FILE__));
+define( 'FACTORY_SHORTCODES_000_VERSION', '3.2.6' );
 
-	#comp merge
-	require(FACTORY_SHORTCODES_000_DIR . '/shortcodes.php');
-	require(FACTORY_SHORTCODES_000_DIR . '/shortcode.class.php');
-	#endcomp
+define( 'FACTORY_SHORTCODES_000_LOADED', true );
+
+define( 'FACTORY_SHORTCODES_000_DIR', dirname( __FILE__ ) );
+
+#comp merge
+require( FACTORY_SHORTCODES_000_DIR . '/shortcodes.php' );
+require( FACTORY_SHORTCODES_000_DIR . '/shortcode.class.php' );
+#endcomp
